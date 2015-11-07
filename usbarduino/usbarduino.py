c681f5a1c5105011dd3c7e5b41c3efcd5f75b1e5
luketi <lukkuti@gmail.com>
2015-11-07 19:00:53 +0000

    usbarduino changes

--- usbarduino/usbarduino.py
+++ usbarduino/usbarduino.py
@@ -34,13 +34,34 @@ c = 0
 while 1 :
     l = ser.readline()
     print(l)
+    msg = l.decode("utf-8")
+    parts = msg.split()
+    
+# DATA PACKET
+# bytes: [protocol version][board id][seq id][app id][recipient board id][value byte 0][value byte 1] ...
+
+    if len(parts) < 6:
+        continue
+
+    ver = int(parts[0])
+    bid = int(parts[1])
+    seq = int(parts[2])
+    app = int(parts[3])
+    rec = int(parts[4])
+
+    if ver > 10:
+        continue
+
+    if app == 4 and len(parts) > 6:
+        lv = int(parts[5]) + int(parts[6]) * 256
+
     c = c + 1
     if c % 10 == 0:
         syslog.syslog('ARDUINO: ' + l.decode("utf-8") )
 
-    if c % 200 == 0:
+    if c % 1000 == 0:
         data = urllib.parse.urlencode({'pid' : '3',
-                         'bid' : '130',
+                         'bid' : '3',
                          'data0' : 1,
                          'data3' : 'arduino.local.heartbeat',
                          'data4' : 'recv data'
